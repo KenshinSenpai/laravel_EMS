1d3892818e1608e74eb296a374bd690936c9f060
Bryan Matubis <116072502+KenshinSenpai@users.noreply.github.com>
2023-03-28 23:56:19 +0800

    Update 2023_03_28_145543_create_participants_table.php

--- migrations/2023_03_28_145543_create_participants_table.php
+++ migrations/2023_03_28_145543_create_participants_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('participants', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('users_id')->constrained()->onDelete('cascade');
             $table->foreignId('event_id')->constrained()->onDelete('cascade');
             $table->dateTime('time_in');
             $table->dateTime('time_out')->nullable();
